866ddd034e5787beeccedfd6ad71167500fb669e
william <william810212@gmail.com>
2021-04-27 12:01:25 +0800

    chore: update base url

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../view/Home.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/liang/'),
   routes: [
     {
       path: '/',
