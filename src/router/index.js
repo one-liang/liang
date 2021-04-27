f0701c5618c88613aa08b6692a71e9b64d785340
william <william810212@gmail.com>
2021-04-27 10:59:12 +0800

    chore: add error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../view/Home.vue';
-import About from '../view/About.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
+      name: 'Home',
       component: Home,
     },
     {
       path: '/about',
-      component: About,
+      name: 'About',
+      component: () => import('../view/About.vue'),
     },
-  ]
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'Error',
+      component: () => import('../view/Error.vue'),
+    },
+  ],
+  scrollBehavior() {
+    return { top: 0 };
+  },
 })
 
 export default router
