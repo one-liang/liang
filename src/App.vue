c646b6e71ce07fc36ee2f98c59657b29a0d4d790
william <william810212@gmail.com>
2021-04-27 10:58:07 +0800

    chore: add footer

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,24 @@
   <main class="px-4">
     <router-view />
   </main>
+  <Footer />
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     Navbar,
+    Footer,
   }
 };
 </script>
 
 <style>
 #app {
+  @apply min-h-screen;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
