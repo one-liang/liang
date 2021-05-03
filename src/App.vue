6293f3c39df61c8c1cac9b65fb033113e8ac225f
william <william810212@gmail.com>
2021-05-03 16:34:13 +0800

    chore: format

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,14 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue';
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Navbar,
     Footer,
-  }
+  },
 };
 </script>
 
