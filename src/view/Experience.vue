b26eb40760b161e7f50526d750c8a175fdc904fe
william <william810212@gmail.com>
2021-05-03 16:34:51 +0800

    chore: add page

---
+++ src/view/Experience.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mx-auto prose">
+    <h1>Experience View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Experience",
+};
+</script>
---
+++ src/view/Portfolio.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mx-auto prose">
+    <h1>Portfolio View</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Portfolio",
+};
+</script>
