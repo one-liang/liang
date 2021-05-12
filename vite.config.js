11cdfa995b2ce0cfefe2e6b8b8d51e1497ef2c8c
william <william810212@gmail.com>
2021-05-12 13:33:00 +0800

    fix: url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/liang/',
+  base: 'https://one-liang.github.io/liang/',
   plugins: [vue()]
 })
