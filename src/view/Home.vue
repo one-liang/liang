063c90a3dc23aa2231a1ea5d0234c42c216d61ef
william <william810212@gmail.com>
2021-05-12 11:35:49 +0800

    chore: update content

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="mx-auto prose">
-    <h1>Home View</h1>
-  </div>
+  <section class="mx-auto prose">
+    <h1 class="!mb-0">簡介</h1>
+    <p class="!mt-1 opacity-80 italic">Intro</p>
+    <p>Hi 我是阿良，是一位熱愛動態效果的前端工程師</p>
+    <p>剛開始接觸網頁是從補習班開始，當時抱持著動態網頁很有趣的心情來學習，學著學著也漸漸入坑，後來也從一些線上課程平台來提升自己，不久離職後找了與網頁相關的工作。</p>
+    <p>學習網頁是興趣的開始，但知道網頁世界裡範圍廣泛，經驗值需一點一點的提升，憑著對網頁的熱情，自主學習和做中學是我學習的宗旨。</p>
+    <br>
+    <h1 class="!mb-0">技能</h1>
+    <p class="!mt-1 opacity-80 italic">Skill</p>
+    <ul>
+      <li>Web 技術：JavaScript、CSS/SCSS、HTML</li>
+      <li>CSS 框架：TailwindCSS、Bootstrap</li>
+      <li>JS 框架：Vue</li>
+      <li>應用框架：Nuxt、Vite</li>
+      <li>版本控管：Github、Gitlab</li>
+    </ul>
+  </section>
 </template>
 
 <script>
