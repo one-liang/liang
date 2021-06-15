8babb73e3b9a4fc357b58919db2007c66d3ce3f9
william <william810212@gmail.com>
2021-06-15 15:19:00 +0800

    deploy

---
+++ assets/Error.js
@@ -0,0 +1,1 @@
+import{c as o,o as r,a as n}from"./vendor.js";const a={name:"Error"},e={class:"mx-auto prose"},s=n("h1",null,"404 not found",-1);a.render=function(n,a,t,u,d,c){return r(),o("div",e,[s])};export default a;
---
+++ assets/mirrorfiction_logo.js
@@ -0,0 +1,1 @@
+var a="/liang/assets/mirrorfiction_logo.232facb9.png";export{a as _};
