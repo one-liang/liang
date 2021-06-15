6c73c3f934bd4806bcb43ec375d114b5a4e17f41
william <william810212@gmail.com>
2021-06-15 15:17:09 +0800

    chore: add vite-plugin-icons

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents from 'vite-plugin-components'
+import Icons,  { ViteIconsResolver } from 'vite-plugin-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +25,9 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    ViteComponents()
+    ViteComponents({
+      customComponentResolvers: ViteIconsResolver(),
+    }),
+    Icons()
   ]
 })
